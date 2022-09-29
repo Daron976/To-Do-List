8baf9defcb8fa4d980b1411344293e9b2e1a940c
Daron Wiafe <daronkwiafe@gmail.com>
2022-09-29 18:40:12 +0200

    Add functionality to web app

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 // eslint-disable-next-line
 import _ from 'lodash';
 import './styles.css';
+import List from './modules/functionality';
 
-const itemInformation = [
-  {
-    id: 1,
-    item: 'Cleaning',
-    status: false,
-  },
-  {
-    id: 2,
-    item: 'Working',
-    status: false,
-  },
-  {
-    id: 3,
-    item: 'Playing',
-    status: false,
-  },
-];
+const newList = new List();
 
-for (let i = 0; i < itemInformation.length; i += 1) {
-  document.getElementById('ulist').innerHTML += `
-  <li class="to-do-list-item flex">
-    <div class="checkbox-container">
-      <input type="checkbox" class="checkbox-input" name="${itemInformation[i].item}" id="${itemInformation[i].number}">
-      <label for="${itemInformation[i].item}">${itemInformation[i].item}</label>
-    </div>
-    <i class="font-color fa-solid fa-ellipsis-vertical"></i>
-  </li>`;
-}
+document.getElementById('item').addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    newList.add();
+  }
+});
+
+window.addEventListener('load', () => {
+  newList.display();
+});
---
+++ src/modules/data.js
@@ -0,0 +1,3 @@
+export const addInput = document.querySelector('#item');
+export const trash = document.getElementsByClassName('fa-trash');
+export const dots = document.getElementsByClassName('fa-ellipsis-vertical');
